81f21b0f095b40014428316dad4769d3e0cd4879
laibaSarfraz20 <154825429+laibaSarfraz20@users.noreply.github.com>
2024-05-23 22:07:31 +0500

    new files added

---
+++ app.js
@@ -0,0 +1,66 @@
+//-------------------------------------------  chapter # 2 ---------------------------------------------
+
+//question 1 & 2
+
+var username;
+var myName = "laiba";
+//question 3
+var message;
+message = "Hello World";
+alert(message);
+//question 4
+var studentName = "laiba";
+var studentAge = 21;
+var studentCourse = "food sci and tech";
+var studentUniversity = "jinnah University";
+
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Student Course: " + studentCourse);
+alert("Student University: " + studentUniversity);
+
+//question 5
+
+var pizzaPattern;
+pizzaPattern = "pizza\npizz\npiz\npi\np";
+alert(pizzaPattern);
+
+//question 6
+
+var email = "example@xyz.com";
+var message = "My email address is " + email;
+alert(message);
+
+// question 7
+
+var book = "A smarter way to learn JavaScript";
+var message = "I am reading a book titled \"" + book + "\"";
+alert(message);
+
+// question 9
+
+var decorativeString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(decorativeString);
+document.write(decorativeString);
+//------------------------------------------chapter # 3-------------------------------------
+
+// question 1
+var age = 21;
+alert("I am " + age + " years old");
+
+// question 3
+
+var birthYear = 1990;
+var message = "My birth year is " + birthYear;
+document.getElementById('birthYear-message').textContent = message;
+
+//question 4
+
+var visitorName = "laiba";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+document.write(message);
+
+
